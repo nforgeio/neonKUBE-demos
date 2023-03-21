920930432bf9cdd8827d8c11a11063cd17515c32
Marcus Bowyer <marcus@euarctos.com>
2023-03-20 17:11:12 -0700

    optionally build images

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,7 @@
 param(
-  [string]$clusterId
+  [string]$clusterId,
+  [string]$registry,
+  [switch]$build = $false
 )
 
 if ($clusterId)
@@ -12,37 +14,68 @@ else
   $clusterDomain = $clusterInfo.Domain
 }
 
+if (!$registry)
+{
+  $registry = "neon-registry.$clusterDomain/library"
+}
+
 $mypath = $MyInvocation.MyCommand.Path
 $myDir = Split-Path $mypath -Parent
 $DEMO_DIR=$myDir
 
-cd $DEMO_DIR\Services\hello-world-operator
-dotnet publish hello-world-operator.csproj -c Release -o .\bin\Release\7.0\publish
-unix-text docker-entrypoint.sh
-docker build -t neon-registry.$clusterDomain/library/hello-world-operator .
-docker push neon-registry.$clusterDomain/library/hello-world-operator
+if ($build)
+{
+  cd $DEMO_DIR\Services\hello-world-operator
+  dotnet publish hello-world-operator.csproj -c Release -o .\bin\Release\7.0\publish
+  unix-text docker-entrypoint.sh
+  docker build -t $registry/hello-world-operator .
+}
+else
+{
+  docker pull ghcr.io/nforgeio/hello-world-operator:latest
+  docker tag ghcr.io/nforgeio/hello-world-operator:latest $registry/hello-world-operator:latest
+}
+docker push $registry/hello-world-operator:latest
 
 if (-not $?)
 {
     throw "ERROR: Publish hello-world-operator failed."
 }
 
-cd $DEMO_DIR\Services\hello-world
-dotnet publish hello-world.csproj -c Release -o .\bin\Release\7.0\publish
-unix-text docker-entrypoint.sh
-docker build -t neon-registry.$clusterDomain/library/hello-world .
-docker push neon-registry.$clusterDomain/library/hello-world
+if ($build)
+{
+  cd $DEMO_DIR\Services\hello-world
+  dotnet publish hello-world.csproj -c Release -o .\bin\Release\7.0\publish
+  unix-text docker-entrypoint.sh
+  docker build -t $registry/hello-world .
+}
+else
+{
+  docker pull ghcr.io/nforgeio/hello-world:latest
+  docker tag ghcr.io/nforgeio/hello-world:latest $registry/hello-world:latest
+}
+
+docker push $registry/hello-world
 
 if (-not $?)
 {
     throw "ERROR: Publish hello-world failed."
 }
 
-cd $DEMO_DIR\Services\load-generator
-dotnet publish load-generator.csproj -c Release -o .\bin\Release\7.0\publish
-unix-text docker-entrypoint.sh
-docker build -t neon-registry.$clusterDomain/library/load-generator .
-docker push neon-registry.$clusterDomain/library/load-generator
+if ($build)
+{
+  cd $DEMO_DIR\Services\load-generator
+  dotnet publish load-generator.csproj -c Release -o .\bin\Release\7.0\publish
+  unix-text docker-entrypoint.sh
+  docker build -t $registry/load-generator .
+}
+else 
+{
+  docker pull ghcr.io/nforgeio/load-generator:latest
+  docker tag ghcr.io/nforgeio/load-generator:latest $registry/load-generator:latest
+}
+
+docker push $registry/load-generator
 
 if (-not $?)
 {
